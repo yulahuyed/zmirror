eaacb54cbef8957cd987102ab2aaf5e2763e076d
yulahuyed <yulahuyed@vpsorg.top>
2017-12-28 13:30:41 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,9 +4,9 @@ if [ "${CONFIG_URL}" ]
 then
   wget -O "config.py" "${CONFIG_URL}"
   sed -i "s/'${CONFIG_DOMAIN}'/'${DOMAIN}'/g" config.py
-  gunicorn --bind 0.0.0.0:8080 --workers 2 --worker-connections 100 wsgi:application
 else
   cp /more_configs/config_google_and_zhwikipedia.py /config.py
   sed -i "s/'127.0.0.1'/'${DOMAIN}'/g" config.py
-  gunicorn --bind 0.0.0.0:8080 --workers 2 --worker-connections 100 wsgi:application
-  
+fi
+
+gunicorn --bind 0.0.0.0:8080 --workers 2 --worker-connections 100 wsgi:application
