77c89fc5db372b6dbb0d9d8e352e48db8a179569
yulahuyed <yulahuyed@vpsorg.top>
2017-12-28 13:27:35 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ "${CONFIG_URL}" ]
+then
+  wget -O "config.py" "${CONFIG_URL}"
+  sed -i "s/'${CONFIG_DOMAIN}'/'${DOMAIN}'/g" config.py
+else
+  cp /more_configs/config_google_and_zhwikipedia.py /config.py
+  sed -i "s/'127.0.0.1'/'${DOMAIN}'/g" config.py
+  gunicorn --bind 0.0.0.0:8080 --workers 2 --worker-connections 100 wsgi:application
+  
