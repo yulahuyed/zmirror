68ec8146e7c4a8b84775bf2f65c03e8f243f14cd
yulahuyed <yulahuyed@vpsorg.top>
2017-12-28 15:20:58 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@
 
 if [ "${CONFIG_URL}" ]
 then
-  wget -O "config.py" "${CONFIG_URL}"
-  sed -i "s/'${CONFIG_DOMAIN}'/'${DOMAIN}'/g" config.py
+  wget -O "config.py" "${CONFIG_URL}" -P /home/zmirror/zmirror
+  sed -i "s/'${CONFIG_DOMAIN}'/'${DOMAIN}'/g" /home/zmirror/zmirror/config.py
 else
-  cp /more_configs/config_google_and_zhwikipedia.py /config.py
-  sed -i "s/'127.0.0.1'/'${DOMAIN}'/g" config.py
+  cp /home/zmirror/zmirror/more_configs/config_google_and_zhwikipedia.py /home/zmirror/zmirror/config.py
+  sed -i "s/'127.0.0.1'/'${DOMAIN}'/g" /home/zmirror/zmirror/config.py
 fi
 
 gunicorn --bind 0.0.0.0:8080 --workers 2 --worker-connections 100 wsgi:application
